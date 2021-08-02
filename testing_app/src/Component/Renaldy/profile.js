388a9b79b410c5cad7337e77c01891bf2ef81a05
Ethutami <ethutami@gmail.com>
2021-08-02 21:58:01 +0800

    adding members page

---
+++ testing_app/src/Component/Renaldy/profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RinaldyProfile() {
+    return (
+        <div style={{margin:10}}>
+            <p style={{color :'tomato'}} onClick={()=>alert("Rinaldy's page")}>Rinaldy</p>
+        </div>
+    )
+}
